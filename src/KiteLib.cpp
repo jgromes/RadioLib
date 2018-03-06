6826cdfa19dbb8590a1d55f58360941c6edc21e0
Jan Gromeš <jgromes@users.noreply.github.com>
2018-03-06 21:25:58 +0100

    Removed commented-out code

--- src/KiteLib.cpp
+++ src/KiteLib.cpp
@@ -1,16 +1,5 @@
 #include "KiteLib.h"
 
-/*Kite::Kite(ModuleType moduleA, ModuleType moduleB) {
-  switch(moduleA) {
-    case ModuleType::ESP8266:
-      ESP8266 = new class ESP8266();
-      break;
-    case ModuleType::SX1278:
-      SX1278 = new class SX1278();
-      break;
-  };
-}*/
-
 KiteShield::KiteShield() {
   ModuleA = new Module(KITE_CS_A, KITE_TX_A, KITE_RX_A, KITE_INT_0, KITE_INT_1);
   ModuleB = new Module(KITE_CS_B, KITE_TX_B, KITE_RX_B, KITE_INT_0, KITE_INT_1);
