be079c5ddc8e4b60d48affd8eb0f167ee140da88
Jan Gromeš <jgromes@users.noreply.github.com>
2018-04-02 13:22:06 +0200

    Extracted common serial code into ISerial class

---
+++ src/ISerial.cpp
@@ -0,0 +1,142 @@
+#include "ISerial.h"
+
+ISerial::ISerial() {
+  
+}
+
+void ISerial::begin(long speed) {
+  _mod->ModuleSerial->begin(speed);
+}
+
+bool ISerial::listen() {
+  return(_mod->ModuleSerial->listen());
+}
+
+void ISerial::end() {
+  _mod->ModuleSerial->end();
+}
+
+bool ISerial::isListening() {
+  return(_mod->ModuleSerial->isListening());
+}
+
+bool ISerial::stopListening() {
+  return(_mod->ModuleSerial->stopListening());
+}
+
+bool ISerial::overflow() {
+  return(_mod->ModuleSerial->overflow());
+}
+
+int ISerial::peek() {
+  return(_mod->ModuleSerial->peek());
+}
+
+size_t ISerial::write(uint8_t b) {
+  return(_mod->ModuleSerial->write(b));
+}
+
+int ISerial::read() {
+  return(_mod->ModuleSerial->read());
+}
+
+int ISerial::available() {
+  return(_mod->ModuleSerial->available());
+}
+
+void ISerial::flush() {
+  _mod->ModuleSerial->flush();
+}
+
+size_t ISerial::print(const __FlashStringHelper *ifsh) {
+  return(_mod->ModuleSerial->print(ifsh));
+}
+
+size_t ISerial::print(const String &s) {
+  return(_mod->ModuleSerial->print(s));
+}
+
+size_t ISerial::print(const char str[]) {
+  return(_mod->ModuleSerial->print(str));
+}
+
+size_t ISerial::print(char c) {
+  return(_mod->ModuleSerial->print(c));
+}
+
+size_t ISerial::print(unsigned char b, int base) {
+  return(_mod->ModuleSerial->print(b, base));
+}
+
+size_t ISerial::print(int n, int base) {
+  return(_mod->ModuleSerial->print(n, base));
+}
+
+size_t ISerial::print(unsigned int n, int base) {
+  return(_mod->ModuleSerial->print(n, base));
+}
+
+size_t ISerial::print(long n, int base) {
+  return(_mod->ModuleSerial->print(n, base));
+}
+
+size_t ISerial::print(unsigned long n, int base) {
+  return(_mod->ModuleSerial->print(n, base));
+}
+
+size_t ISerial::print(double n, int digits) {
+  return(_mod->ModuleSerial->print(n, digits));
+}
+
+size_t ISerial::print(const Printable& x) {
+  return(_mod->ModuleSerial->print(x));
+}
+
+size_t ISerial::println(const __FlashStringHelper *ifsh) {
+  return(_mod->ModuleSerial->println(ifsh));
+}
+
+size_t ISerial::println(const String &s) {
+  return(_mod->ModuleSerial->println(s));
+}
+
+size_t ISerial::println(const char str[]) {
+  return(_mod->ModuleSerial->println(str));
+}
+
+size_t ISerial::println(char c) {
+  return(_mod->ModuleSerial->println(c));
+}
+
+size_t ISerial::println(unsigned char b, int base) {
+  return(_mod->ModuleSerial->println(b, base));
+}
+
+size_t ISerial::println(int n, int base) {
+  return(_mod->ModuleSerial->println(n, base));
+}
+
+size_t ISerial::println(unsigned int n, int base) {
+  return(_mod->ModuleSerial->println(n, base));
+}
+
+size_t ISerial::println(long n, int base) {
+  return(_mod->ModuleSerial->println(n, base));
+}
+
+size_t ISerial::println(unsigned long n, int base) {
+  return(_mod->ModuleSerial->println(n, base));
+}
+
+size_t ISerial::println(double n, int digits) {
+  return(_mod->ModuleSerial->println(n, digits));
+}
+
+size_t ISerial::println(const Printable& x) {
+  return(_mod->ModuleSerial->println(x));
+}
+
+size_t ISerial::println(void) {
+  return(_mod->ModuleSerial->println());
+}
+  
