b772fc915d1c8fe3eb571ae721e8fd69553a3ed0
Jan Gromeš <jgromes@users.noreply.github.com>
2018-07-23 12:43:12 +0200

    HC05 - updated example

--- examples/HC05/HC05.ino
+++ examples/HC05/HC05.ino
@@ -1,10 +1,10 @@
 /*
- * KiteLib HC05 Example
- * 
- * This example sends data using HC05 Bluetooth module.
- * HC05 works exactly like a Serial line, data are sent to the paired device.
- * The default pairing code for HC05 is 1234.
- */
+   KiteLib HC05 Example
+
+   This example sends data using HC05 Bluetooth module.
+   HC05 works exactly like a Serial line, data are sent to the paired device.
+   The default pairing code for HC05 is 1234.
+*/
 
 // include the library
 #include <KiteLib.h>
@@ -23,12 +23,12 @@ void setup() {
 void loop() {
   // HC05 supports all methods of the Serial class
   // read data incoming from Serial port and write them to Bluetooth
-  while(Serial.available() > 0) {
+  while (Serial.available() > 0) {
     bluetooth.write(Serial.read());
   }
-  
+
   // read data incoming from Bluetooth and write them to Serial port
-  while(bluetooth.available() > 0) {
+  while (bluetooth.available() > 0) {
     Serial.write(bluetooth.read());
   }
 }
