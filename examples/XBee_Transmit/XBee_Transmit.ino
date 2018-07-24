305ee6e6ed4e9871c5e09c9003a98913cf82e4e3
Jan Gromeš <jgromes@users.noreply.github.com>
2018-07-24 09:47:05 +0200

    XBee - updated example

--- examples/XBee_Transmit/XBee_Transmit.ino
+++ examples/XBee_Transmit/XBee_Transmit.ino
@@ -1,12 +1,12 @@
 /*
- * KiteLib XBee API Transmit Example
- * 
- * This example transmits packets using XBee API mode.
- * In API mode, many XBee modules can form a mesh network.
- * 
- * IMPORTANT: Before uploading this example, make sure that the XBee module 
- * is running API COORDINATOR firmware!
- */
+   KiteLib XBee API Transmit Example
+
+   This example transmits packets using XBee API mode.
+   In API mode, many XBee modules can form a mesh network.
+
+   IMPORTANT: Before uploading this example, make sure that the XBee module
+   is running API COORDINATOR firmware!
+*/
 
 // include the library
 #include <KiteLib.h>
@@ -20,38 +20,40 @@ void setup() {
   // initialize XBee module with baudrate 9600
   Serial.print(F("[XBee] Initializing ... "));
   int state = bee.begin(9600);
-  if(state == ERR_NONE) {
+  if (state == ERR_NONE) {
     Serial.println(F("success!"));
   } else {
     Serial.print(F("failed, code "));
     Serial.println(state);
-    while(true);
+    while (true);
   }
 
   // set PAN ID to 0x0123456789ABCDEF
   Serial.print(F("[XBee] Setting PAN ID ... "));
-  uint8_t panId[] = {0x01, 0x23, 0x45, 0x67, 
+  uint8_t panId[] = {0x01, 0x23, 0x45, 0x67,
                      0x89, 0xAB, 0xCD, 0xEF};
   state = bee.setPanId(panId);
-  if(state == ERR_NONE) {
+  if (state == ERR_NONE) {
     Serial.println(F("success!"));
   } else {
     Serial.print(F("failed, code "));
     Serial.println(state);
-    while(true);
+    while (true);
   }
 }
 
 void loop() {
   // transmit data to the destination module
-  uint8_t dest[] = {0x00, 0x13, 0xA2, 0x00, 
+  uint8_t dest[] = {0x00, 0x13, 0xA2, 0x00,
                     0x40, 0xA5, 0x8A, 0x6B};
   Serial.print(F("[XBee] Transmitting message ... "));
   int state = bee.transmit(dest, "Hello World!");
-  if(state == ERR_NONE) {
+  if (state == ERR_NONE) {
     Serial.println(F("success!"));
   } else {
     Serial.print(F("failed, code "));
     Serial.println(state);
   }
+
+  delay(1000);
 }
