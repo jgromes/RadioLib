1f6c3702301500fdc4337484e5fef1fd99222d74
jgromes <jgromes@users.noreply.github.com>
2019-02-09 12:03:22 +0100

    [CC1101] Updated example

--- examples/CC1101_Transmit/CC1101_Transmit.ino
+++ examples/CC1101_Transmit/CC1101_Transmit.ino
@@ -16,27 +16,27 @@ void setup() {
   // initialize CC1101
   Serial.print(F("[CC1101] Initializing ... "));
   // carrier frequency:                   868.0 MHz
-  // bit rate:                            115.2 kbps
-  // Rx bandwidth:                        203 kHz
+  // bit rate:                            4.8 kbps
+  // Rx bandwidth:                        325.0 kHz
   // frequency deviation:                 48.0 kHz
   // sync word:                           0xD391
   int state = cc.begin();
-  if(state == ERR_NONE) {
+  if (state == ERR_NONE) {
     Serial.println(F("success!"));
   } else {
     Serial.print(F("failed, code "));
     Serial.println(state);
-    while(true);
+    while (true);
   }
 }
 
 void loop() {
   Serial.print(F("[CC1101] Transmitting packet ... "));
 
-  // you can transmit C-string or Arduino string up to 255 characters long
+  // you can transmit C-string or Arduino string up to 63 characters long
   int state = cc.transmit("Hello World!");
 
-  // you can also transmit byte array up to 255 bytes long
+  // you can also transmit byte array up to 63 bytes long
   /*
     byte byteArr[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
     int state = cc.transmit(byteArr, 8);
