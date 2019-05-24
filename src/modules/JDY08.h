804c6b3d894c40e254600af930a37a4c504305bc
jgromes <jan.gromes@gmail.com>
2019-05-24 21:26:00 +0200

    [JDY08] Added Doxygen comments

--- src/modules/JDY08.h
+++ src/modules/JDY08.h
@@ -3,12 +3,26 @@
 
 #include "ISerial.h"
 
+/*!
+  \class JDY08
+
+  \brief Control class for %JDY08 module.
+  Most methods supported by this module are implemented in ISerial interface.
+*/
 class JDY08: public ISerial {
   public:
-    // constructor
+    /*!
+      \brief Default constructor.
+
+      \param mod Instance of Module that will be used to communicate with the radio.
+    */
     JDY08(Module* mod);
-    
-    // basic methods
+
+    /*!
+      \brief Initialization method.
+
+      \param speed Baud rate to use for UART interface.
+    */
     void begin(long speed);
 };
 
