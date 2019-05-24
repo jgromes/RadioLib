4a67bb59c7bdcd9de534604b05f0688d168c844d
jgromes <jan.gromes@gmail.com>
2019-05-24 21:40:56 +0200

    [SX1231] Added Doxygen comments

--- src/modules/SX1231.h
+++ src/modules/SX1231.h
@@ -15,14 +15,37 @@
 //SX1231_REG_TEST_OOK
 #define SX1231_OOK_DELTA_THRESHOLD                    0x0C
 
+/*!
+  \class SX1231
+
+  \brief Control class for %SX1231 module. Overrides some methods from RF69 due to different register values.
+*/
 class SX1231: public RF69  {
   public:
-    // constructor
+    /*!
+      \brief Default constructor.
+
+      \param mod Instance of Module that will be used to communicate with the radio.
+    */
     SX1231(Module* mod);
-    
-    // basic methods
+
+    /*!
+      \brief Initialization method.
+
+      \param freq Carrier frequency in MHz. Defaults to 434.0 MHz.
+
+      \param br Bit rate to be used in kbps. Defaults to 48.0 kbps.
+
+      \param rxBw Receiver bandwidth in kHz. Defaults to 125.0 kHz.
+
+      \param freqDev Frequency deviation from carrier frequency in kHz Defaults to 50.0 kHz.
+
+      \param power Output power in dBm. Defaults to 13 dBm.
+
+      \returns \ref status_codes
+    */
     int16_t begin(float freq = 434.0, float br = 48.0, float rxBw = 125.0, float freqDev = 50.0, int8_t power = 13);
-  
+
   private:
     uint8_t _chipRevision;
 };
