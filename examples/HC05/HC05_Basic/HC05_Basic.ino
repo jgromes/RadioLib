0adf34fea24dee77170a9ad3128787406767af12
jgromes <jgromes@users.noreply.github.com>
2019-06-02 15:02:12 +0200

    [HC05] Updated examples

--- examples/HC05/HC05_Basic/HC05_Basic.ino
+++ examples/HC05/HC05_Basic/HC05_Basic.ino
@@ -3,14 +3,23 @@
 
    This example sends data using HC05 Bluetooth module.
    HC05 works exactly like a Serial line, data are sent to the paired device.
-   The default pairing code for HC05 is 1234.
+   The default pairing code for HC05 is 1234 or 1111.
+
+   For full API reference, see the GitHub Pages
+   https://jgromes.github.io/RadioLib/
 */
 
 // include the library
 #include <RadioLib.h>
 
-// HC05 module is in slot A on the shield
-HC05 bluetooth = RadioShield.ModuleA;
+// HC05 has the following connections:
+// TX pin: 9
+// RX pin: 8
+HC05 bluetooth = new Module(9, 8);
+
+// or using RadioShield
+// https://github.com/jgromes/RadioShield
+//HC05 bluetooth = RadioShield.ModuleA;
 
 void setup() {
   Serial.begin(9600);
