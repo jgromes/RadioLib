4b9de4c290503670338713c09f532c8a56616eef
jgromes <jgromes@users.noreply.github.com>
2019-06-02 15:02:26 +0200

    [JDY08] Updated examples

--- examples/JDY08/JDY08_Basic/JDY08_Basic.ino
+++ examples/JDY08/JDY08_Basic/JDY08_Basic.ino
@@ -2,13 +2,23 @@
    RadioLib JDY08 Example
 
    This example sends data using JDY08 Bluetooth module.
+   JDY08 works exactly like a Serial line, data are sent to the paired device.
+
+   For full API reference, see the GitHub Pages
+   https://jgromes.github.io/RadioLib/
 */
 
 // include the library
 #include <RadioLib.h>
 
-// JDY08 module is in slot A on the shield
-JDY08 ble = RadioShield.ModuleA;
+// JDY08 has the following connections:
+// TX pin: 9
+// RX pin: 8
+JDY08 ble = new Module(9, 8);
+
+// or using RadioShield
+// https://github.com/jgromes/RadioShield
+//JDY08 ble = RadioShield.ModuleA;
 
 void setup() {
   Serial.begin(9600);
