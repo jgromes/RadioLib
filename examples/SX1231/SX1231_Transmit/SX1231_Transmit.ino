2f4a5c660c1f6bf74ae457ea7c0e800574dbb309
jgromes <jgromes@users.noreply.github.com>
2020-03-22 08:11:55 +0100

    [SX1231] Fixed typos

--- examples/SX1231/SX1231_Transmit/SX1231_Transmit.ino
+++ examples/SX1231/SX1231_Transmit/SX1231_Transmit.ino
@@ -53,17 +53,17 @@ void loop() {
 
   // you can also transmit byte array up to 256 bytes long
   /*
-    byte byteArr[] = {0x01, 0x23, 0x45, 0x56, 0x78, 0xAB, 0xCD, 0xEF};
+    byte byteArr[] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
     int state = rf.transmit(byteArr, 8);
   */
 
   if (state == ERR_NONE) {
     // the packet was successfully transmitted
-    Serial.println(F(" success!"));
+    Serial.println(F("success!"));
 
   } else if (state == ERR_PACKET_TOO_LONG) {
     // the supplied packet was longer than 256 bytes
-    Serial.println(F(" too long!"));
+    Serial.println(F("too long!"));
 
   }
 
