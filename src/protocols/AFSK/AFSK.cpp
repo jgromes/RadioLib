79d8ff2ca03a43689f5be8662a8027e8ccc4bf3d
jgromes <jgromes@users.noreply.github.com>
2020-05-03 11:04:51 +0200

    [AFSK] Added check for zero tone frequency

--- src/protocols/AFSK/AFSK.cpp
+++ src/protocols/AFSK/AFSK.cpp
@@ -6,6 +6,10 @@ AFSKClient::AFSKClient(PhysicalLayer* phy, RADIOLIB_PIN_TYPE pin) {
 }
 
 int16_t AFSKClient::tone(uint16_t freq, bool autoStart) {
+  if(freq == 0) {
+    return(ERR_INVALID_FREQUENCY);
+  }
+
   if(autoStart) {
     int16_t state = _phy->transmitDirect();
     RADIOLIB_ASSERT(state);
