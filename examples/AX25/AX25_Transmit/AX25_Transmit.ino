41db6b18bfbe20f84e4ef20af389f2f6b4730bd5
jgromes <jgromes@users.noreply.github.com>
2020-07-04 12:02:47 +0200

    [AX25] Added links to default config wiki page

--- examples/AX25/AX25_Transmit/AX25_Transmit.ino
+++ examples/AX25/AX25_Transmit/AX25_Transmit.ino
@@ -13,6 +13,9 @@
     - nRF24
     - Si443x/RFM2x
 
+   For default module settings, see the wiki page
+   https://github.com/jgromes/RadioLib/wiki/Default-configuration
+
    For full API reference, see the GitHub Pages
    https://jgromes.github.io/RadioLib/
 */
@@ -25,14 +28,14 @@
 // DIO0 pin:  2
 // RESET pin: 9
 // DIO1 pin:  3
-SX1278 fsk = new Module(10, 2, 9, 3);
+SX1278 radio = new Module(10, 2, 9, 3);
 
 // or using RadioShield
 // https://github.com/jgromes/RadioShield
-//SX1278 fsk = RadioShield.ModuleA;
+//SX1278 radio = RadioShield.ModuleA;
 
 // create AX.25 client instance using the FSK module
-AX25Client ax25(&fsk);
+AX25Client ax25(&radio);
 
 void setup() {
   Serial.begin(9600);
@@ -42,11 +45,11 @@ void setup() {
   // carrier frequency:           434.0 MHz
   // bit rate:                    1.2 kbps (1200 baud 2-FSK AX.25)
   // frequency deviation:         0.5 kHz  (1200 baud 2-FSK AX.25)
-  int state = fsk.beginFSK(434.0, 1.2, 0.5);
+  int state = radio.beginFSK(434.0, 1.2, 0.5);
 
   // when using one of the non-LoRa modules for AX.25
   // (RF69, CC1101,, Si4432 etc.), use the basic begin() method
-  // int state = fsk.begin();
+  // int state = radio.begin();
 
   if(state == ERR_NONE) {
     Serial.println(F("success!"));
