8d92a5f402ef2609b4ba3b7e2ca3df0502292bfa
jgromes <jgromes@users.noreply.github.com>
2020-07-18 07:09:33 +0200

    [CC1101] Fixed inverted check (#167)

--- src/modules/CC1101/CC1101.cpp
+++ src/modules/CC1101/CC1101.cpp
@@ -198,7 +198,7 @@ void CC1101::clearGdo0Action() {
 }
 
 void CC1101::setGdo2Action(void (*func)(void), RADIOLIB_INTERRUPT_STATUS dir) {
-  if(_mod->getGpio() != RADIOLIB_NC) {
+  if(_mod->getGpio() == RADIOLIB_NC) {
     return;
   }
   Module::pinMode(_mod->getGpio(), INPUT);
@@ -206,7 +206,7 @@ void CC1101::setGdo2Action(void (*func)(void), RADIOLIB_INTERRUPT_STATUS dir) {
 }
 
 void CC1101::clearGdo2Action() {
-  if(_mod->getGpio() != RADIOLIB_NC) {
+  if(_mod->getGpio() == RADIOLIB_NC) {
     return;
   }
   detachInterrupt(RADIOLIB_DIGITAL_PIN_TO_INTERRUPT(_mod->getGpio()));
