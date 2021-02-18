f16f8e43f46a69591701ee7e7bc4fb34fc1bfb79
jgromes <jgromes@users.noreply.github.com>
2021-02-18 18:48:01 +0100

    [RFM9x] Fixed frequency setting not saved (#253)

--- src/modules/RFM9x/RFM95.cpp
+++ src/modules/RFM9x/RFM95.cpp
@@ -43,8 +43,12 @@ int16_t RFM95::begin(float freq, float bw, uint8_t sf, uint8_t cr, uint8_t syncW
 int16_t RFM95::setFrequency(float freq) {
   RADIOLIB_CHECK_RANGE(freq, 862.0, 1020.0, ERR_INVALID_FREQUENCY);
 
-  // set frequency
-  return(SX127x::setFrequencyRaw(freq));
+  // set frequency and if successful, save the new setting
+  int16_t state = SX127x::setFrequencyRaw(freq);
+  if(state == ERR_NONE) {
+    SX127x::_freq = freq;
+  }
+  return(state);
 }
 
 #endif
--- src/modules/RFM9x/RFM96.cpp
+++ src/modules/RFM9x/RFM96.cpp
@@ -44,8 +44,12 @@ int16_t RFM96::begin(float freq, float bw, uint8_t sf, uint8_t cr, uint8_t syncW
 int16_t RFM96::setFrequency(float freq) {
   RADIOLIB_CHECK_RANGE(freq, 410.0, 525.0, ERR_INVALID_FREQUENCY);
 
-  // set frequency
-  return(SX127x::setFrequencyRaw(freq));
+  // set frequency and if successful, save the new setting
+  int16_t state = SX127x::setFrequencyRaw(freq);
+  if(state == ERR_NONE) {
+    SX127x::_freq = freq;
+  }
+  return(state);
 }
 
 #endif
