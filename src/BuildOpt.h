24ea845c7c21c3d5df0b3816f902d3528a2e2802
jgromes <jan.gromes@gmail.com>
2021-11-27 18:20:59 +0100

    Bump version to 5.0.0

--- src/BuildOpt.h
+++ src/BuildOpt.h
@@ -922,8 +922,8 @@
 #endif
 
 // version definitions
-#define RADIOLIB_VERSION_MAJOR  (0x04)
-#define RADIOLIB_VERSION_MINOR  (0x06)
+#define RADIOLIB_VERSION_MAJOR  (0x05)
+#define RADIOLIB_VERSION_MINOR  (0x00)
 #define RADIOLIB_VERSION_PATCH  (0x00)
 #define RADIOLIB_VERSION_EXTRA  (0x00)
 
