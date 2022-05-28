cedd5b05120d9790ad72b0154f0f99289cde6cf5
jgromes <jgromes@users.noreply.github.com>
2022-05-28 16:23:05 +0000

    Deploying to gh-pages from  @ 3df3b092ebf412bd0b26524e7b296733bd6a62f7 🚀

--- class_s_x1262.js
+++ class_s_x1262.js
@@ -2,7 +2,7 @@ var class_s_x1262 =
 [
     [ "SX1262", "class_s_x1262.html#a0da317728ec8ef23c5032d550c9acb8d", null ],
     [ "begin", "class_s_x1262.html#a9ceab9913d102c2fd657a1a91afaf9cc", null ],
-    [ "beginFSK", "class_s_x1262.html#a85dadad94eb55ea62a9626a2602e3bde", null ],
+    [ "beginFSK", "class_s_x1262.html#a36d2c94ff9c3b9126fde23e3c54630f1", null ],
     [ "setFrequency", "class_s_x1262.html#a7e72da22fa1fc2d87186107a3285e846", null ],
     [ "setOutputPower", "class_s_x1262.html#aa149463283dc9cddfec836ec6620d4dc", null ]
 ];
