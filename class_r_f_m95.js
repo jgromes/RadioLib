70574cab928140eb9ad461831050881ef3a50323
jgromes <jgromes@users.noreply.github.com>
2022-08-20 17:02:27 +0000

    Deploying to gh-pages from  @ bedbf7bb947182734478e72c3ab84167b060413b 🚀

--- class_r_f_m95.js
+++ class_r_f_m95.js
@@ -2,6 +2,6 @@ var class_r_f_m95 =
 [
     [ "RFM95", "class_r_f_m95.html#a89dfea02aef1a2b47a3af83801c74326", null ],
     [ "begin", "class_r_f_m95.html#a5c37eaf6ae8c558a9a623157dd9f894a", null ],
-    [ "beginFSK", "class_r_f_m95.html#a878cb6f8df95b3bdfa4553384a43e974", null ],
+    [ "beginFSK", "class_r_f_m95.html#a7e0f8fa59ddd48f7b026b0f996202b30", null ],
     [ "setFrequency", "class_r_f_m95.html#a9dbe60f998ddc661282ebf454dba0f87", null ]
 ];
