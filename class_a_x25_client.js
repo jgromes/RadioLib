dac9bcd40ee4a3d351be9807f323474248735358
jgromes <jgromes@users.noreply.github.com>
2022-10-23 08:20:00 +0000

    Deploying to gh-pages from  @ 67937f4f46d94c2c79129acc74f0fc6db7d836e8 🚀

--- class_a_x25_client.js
+++ class_a_x25_client.js
@@ -4,7 +4,7 @@ var class_a_x25_client =
     [ "AX25Client", "class_a_x25_client.html#a6e81e629817cdf1b377e4b4f7e4d6520", null ],
     [ "begin", "class_a_x25_client.html#a38be2b9385e4804339b3e4b57b90c2ca", null ],
     [ "sendFrame", "class_a_x25_client.html#a341db993853c6817a8f6c7087ead0ba6", null ],
-    [ "setCorrection", "class_a_x25_client.html#aa6a10784d59428a4d5c13067ba6af69c", null ],
+    [ "setCorrection", "class_a_x25_client.html#a74a43c60c84e4150b9508c83e461d24e", null ],
     [ "transmit", "class_a_x25_client.html#a985790943f3f3e06a2dfdd36977d0b98", null ],
     [ "transmit", "class_a_x25_client.html#ada21695e373b3e9b0e0f5d3cfbf29826", null ],
     [ "APRSClient", "class_a_x25_client.html#ac7d2ea7e69067b7b4c6492e198505289", null ]
