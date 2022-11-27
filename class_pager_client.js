39d7d775ae73e93a2949b5447a5c2ec0e9b66687
jgromes <jgromes@users.noreply.github.com>
2022-11-27 19:47:51 +0000

    Deploying to gh-pages from  @ f51229ef9a0be16079360641843edf219e0ad112 🚀

--- class_pager_client.js
+++ class_pager_client.js
@@ -2,7 +2,7 @@ var class_pager_client =
 [
     [ "PagerClient", "class_pager_client.html#a9f978120467b13104fb356e9b7d855ec", null ],
     [ "available", "class_pager_client.html#aec073fa9e5adcff9730482d9583715e9", null ],
-    [ "begin", "class_pager_client.html#a7c9b5107f17b181ad8c99b6fc5046ee5", null ],
+    [ "begin", "class_pager_client.html#a1a1dc569f9b60b0ddd6ead7af5a8244f", null ],
     [ "readData", "class_pager_client.html#ac000d0d6b1eb389ead95290f8895d02d", null ],
     [ "readData", "class_pager_client.html#ad2c6132819a84712b929c0bb22ba172b", null ],
     [ "sendTone", "class_pager_client.html#a8f9af4c0a5c2e9de7cdfa1a907479111", null ],
