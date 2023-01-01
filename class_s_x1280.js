84fa52e386f3aec9c32ad844a1e41307dc9cd986
jgromes <jgromes@users.noreply.github.com>
2023-01-01 17:31:57 +0000

    Deploying to gh-pages from  @ a0884bf1207c4eea0618c435ef0cd27deeea417d 🚀

--- class_s_x1280.js
+++ class_s_x1280.js
@@ -2,6 +2,6 @@ var class_s_x1280 =
 [
     [ "SX1280", "class_s_x1280.html#a0356199b89860e15cda4979cd9dc13eb", null ],
     [ "getRangingResult", "class_s_x1280.html#a91f4f241f02cd4e79d15a9ba08eb1e8f", null ],
-    [ "range", "class_s_x1280.html#a812e289084b2f78977b254c28f0fff7c", null ],
-    [ "startRanging", "class_s_x1280.html#af30ff497ca3bcc043dc4dc2e7587a795", null ]
+    [ "range", "class_s_x1280.html#a3112e6bce39e6534711f5d76449f3dd2", null ],
+    [ "startRanging", "class_s_x1280.html#a78cebe909fb2776f11f6c50f4f241c10", null ]
 ];
