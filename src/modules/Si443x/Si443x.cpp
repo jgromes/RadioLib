bb17b3dff289080abed0b5e67a7db128fcb5001c
jgromes <jan.gromes@gmail.com>
2023-05-21 18:25:07 +0200

    [Si443x] Fixed long prints

--- src/modules/Si443x/Si443x.cpp
+++ src/modules/Si443x/Si443x.cpp
@@ -754,7 +754,7 @@ int16_t Si443x::updateClockRecovery() {
 
   // print that whole mess
   RADIOLIB_DEBUG_PRINTLN("%X\n%X\n%X", bypass, decRate, manch);
-  RADIOLIB_DEBUG_PRINTLN("%f\t%d\t%X\n%d\t%X\n%d\t%X", rxOsr, rxOsr_fixed, rxOsr_fixed, ncoOff, ncoOff, crGain, crGain);
+  RADIOLIB_DEBUG_PRINTLN("%f\t%d\t%X\n%lu\t%lX\n%d\t%X", rxOsr, rxOsr_fixed, rxOsr_fixed, ncoOff, ncoOff, crGain, crGain);
 
   // update oversampling ratio
   int16_t state = this->mod->SPIsetRegValue(RADIOLIB_SI443X_REG_CLOCK_REC_OFFSET_2, (uint8_t)((rxOsr_fixed & 0x0700) >> 3), 7, 5);
