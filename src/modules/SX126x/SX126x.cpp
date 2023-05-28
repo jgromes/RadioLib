2b28296c942fd4c624414924cb259d0b76a68f1b
jgromes <jan.gromes@gmail.com>
2023-05-28 22:20:54 +0200

    [SX128x] Added default startReceive method

--- src/modules/SX126x/SX126x.cpp
+++ src/modules/SX126x/SX126x.cpp
@@ -539,6 +539,10 @@ int16_t SX126x::finishTransmit() {
   return(standby());
 }
 
+int16_t SX126x::startReceive() {
+  return(this->startReceive(RADIOLIB_SX126X_RX_TIMEOUT_INF, RADIOLIB_SX126X_IRQ_RX_DEFAULT, RADIOLIB_SX126X_IRQ_RX_DONE, 0));
+}
+
 int16_t SX126x::startReceive(uint32_t timeout, uint16_t irqFlags, uint16_t irqMask, size_t len) {
   (void)len;
   int16_t state = startReceiveCommon(timeout, irqFlags, irqMask);
