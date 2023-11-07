d262732554e2d1266d543182ac1be62a8e3bcc3e
jgromes <jan.gromes@gmail.com>
2023-11-07 07:13:02 +0100

    [RFM9x] Added missing alias for RFM95

--- src/modules/SX127x/SX1276.h
+++ src/modules/SX127x/SX1276.h
@@ -70,6 +70,12 @@ class SX1276: public SX1278 {
 };
 
 /*!
+  \class RFM95
+  \brief Only exists as alias for SX1276, since there seems to be no difference between %RFM95 and %SX1276 modules.
+*/
+RADIOLIB_TYPE_ALIAS(SX1276, RFM95);
+
+/*!
   \class RFM96
   \brief Only exists as alias for SX1276, since there seems to be no difference between %RFM96 and %SX1276 modules.
 */
