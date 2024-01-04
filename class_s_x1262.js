a712f7be6ae89eb28a79657882841d7b30aa458a
jgromes <jgromes@users.noreply.github.com>
2024-01-04 17:30:49 +0000

    Deploying to gh-pages from  @ bf061c655ffee2373c90cade51253cf0fe466c8d 🚀

--- class_s_x1262.js
+++ class_s_x1262.js
@@ -4,6 +4,6 @@ var class_s_x1262 =
     [ "begin", "class_s_x1262.html#a9ceab9913d102c2fd657a1a91afaf9cc", null ],
     [ "beginFSK", "class_s_x1262.html#a36d2c94ff9c3b9126fde23e3c54630f1", null ],
     [ "setFrequency", "class_s_x1262.html#abb018b820eb71ca6b0cd92c62affa767", null ],
-    [ "setFrequency", "class_s_x1262.html#a1f70b082116b03d1fafe395fc5a288f6", null ],
+    [ "setFrequency", "class_s_x1262.html#a48794e87684bc70b8e987066d827a0fd", null ],
     [ "setOutputPower", "class_s_x1262.html#aa149463283dc9cddfec836ec6620d4dc", null ]
 ];
--- class_s_x1268.js
+++ class_s_x1268.js
@@ -4,6 +4,6 @@ var class_s_x1268 =
     [ "begin", "class_s_x1268.html#ad9e92b39ae0fdfa47131ddf7adb92b71", null ],
     [ "beginFSK", "class_s_x1268.html#af6b041392136b599eec57085e2067a6f", null ],
     [ "setFrequency", "class_s_x1268.html#abf338b7036d692a4a8ca6bf305c0cb12", null ],
-    [ "setFrequency", "class_s_x1268.html#a59b538f4971525849b7aaa3456de9929", null ],
+    [ "setFrequency", "class_s_x1268.html#a8bb9dac1694ca41e7d6b4b93deff2b1e", null ],
     [ "setOutputPower", "class_s_x1268.html#a5b0744aa46fbb4f8c738b010dfcc9b45", null ]
 ];
