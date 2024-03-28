fb35416b346e0a759d2c50cb0f94bf2e36746ce1
Crsarmv7l <85343771+Crsarmv7l@users.noreply.github.com>
2024-03-28 13:19:18 -0400

    Fix ()

--- src/modules/CC1101/CC1101.cpp
+++ src/modules/CC1101/CC1101.cpp
@@ -492,9 +492,9 @@ int16_t CC1101::setRxBandwidth(float rxBw) {
 int16_t CC1101::autoSetRxBandwidth() {
     // Uncertainty ~ +/- 40ppm for a cheap CC1101
     // Uncertainty * 2 for both transmitter and receiver
-    float uncertainty = (this->frequency) * 40 * 2);
+    float uncertainty = ((this->frequency) * 40 * 2);
     uncertainty = (uncertainty/1000); //Since bitrate is in kBit
-    float minbw = (this->bitRate) + uncertainty);
+    float minbw = ((this->bitRate) + uncertainty);
     
     int possibles[16] = {58, 68, 81, 102, 116, 135, 162, 203, 232, 270, 325, 406, 464, 541, 650, 812};
     
