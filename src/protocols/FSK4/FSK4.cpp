05e35407f46c498df305db08781beb030b886f26
jgromes <jan.gromes@gmail.com>
2024-05-01 15:14:44 +0200

    [FSK4] Use abs macro instead of abs function

--- src/protocols/FSK4/FSK4.cpp
+++ src/protocols/FSK4/FSK4.cpp
@@ -1,5 +1,4 @@
 #include "FSK4.h"
-#include <stdlib.h>
 #include <math.h>
 #if !RADIOLIB_EXCLUDE_FSK4
 
@@ -113,12 +112,12 @@ int32_t FSK4Client::getRawShift(int32_t shift) {
   int32_t step = round(phyLayer->getFreqStep());
 
   // check minimum shift value
-  if(abs(shift) < step / 2) {
+  if(RADIOLIB_ABS(shift) < step / 2) {
     return(0);
   }
 
   // round shift to multiples of frequency step size
-  if(abs(shift) % step < (step / 2)) {
+  if(RADIOLIB_ABS(shift) % step < (step / 2)) {
     return(shift / step);
   }
   if(shift < 0) {
