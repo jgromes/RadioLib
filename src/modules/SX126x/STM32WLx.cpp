98c80a9cb97f8ee19435ee94b01e934aa2b5a280
jgromes <jan.gromes@gmail.com>
2024-05-19 19:05:03 +0200

    [STM32WL] Fix output ramp time configuration

--- src/modules/SX126x/STM32WLx.cpp
+++ src/modules/SX126x/STM32WLx.cpp
@@ -93,9 +93,8 @@ int16_t STM32WLx::setOutputPower(int8_t power) {
   state = SX126x::fixPaClamping(use_hp);
   RADIOLIB_ASSERT(state);
 
-  // set output power
-  /// \todo power ramp time configuration
-  state = SX126x::setTxParams(power);
+  // set output power with default 200us ramp
+  state = SX126x::setTxParams(power, RADIOLIB_SX126X_PA_RAMP_200U);
   RADIOLIB_ASSERT(state);
 
   // restore OCP configuration
