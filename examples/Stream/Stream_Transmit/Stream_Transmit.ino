b34be58e1a22b185758ea14e4136237245c65105
jgromes <jan.gromes@gmail.com>
2024-06-29 18:33:34 +0200

    [Stream] Added delay to halting while loops

--- examples/Stream/Stream_Transmit/Stream_Transmit.ino
+++ examples/Stream/Stream_Transmit/Stream_Transmit.ino
@@ -65,7 +65,7 @@ void setup() {
   } else {
     Serial.print(F("failed, code "));
     Serial.println(state);
-    while (true);
+    while (true) { delay(10); }
   }
 
   // set the function that will be called
