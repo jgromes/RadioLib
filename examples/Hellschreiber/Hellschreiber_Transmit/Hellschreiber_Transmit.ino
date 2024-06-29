83bf00358990027c8a765de7f0b0407067705c6e
jgromes <jan.gromes@gmail.com>
2024-06-29 18:31:27 +0200

    [Hell] Added delay to halting while loops

--- examples/Hellschreiber/Hellschreiber_Transmit/Hellschreiber_Transmit.ino
+++ examples/Hellschreiber/Hellschreiber_Transmit/Hellschreiber_Transmit.ino
@@ -55,7 +55,7 @@ void setup() {
   } else {
     Serial.print(F("failed, code "));
     Serial.println(state);
-    while(true);
+    while (true) { delay(10); }
   }
 
   // initialize Hellschreiber client
@@ -68,7 +68,7 @@ void setup() {
   } else {
     Serial.print(F("failed, code "));
     Serial.println(state);
-    while(true);
+    while (true) { delay(10); }
   }
 }
 
