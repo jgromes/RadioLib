8eb6f0cf24c01fb8ff3381d1279425e8b9ac9d54
Walter Dunckel <radiohound@gmail.com>
2024-11-19 06:30:10 -0800

    Change order of setFlag function
    The change allows the code to compile. The same issue exists with a few other examples too.

--- examples/STM32WLx/STM32WLx_Transmit_Interrupt/STM32WLx_Transmit_Interrupt.ino
+++ examples/STM32WLx/STM32WLx_Transmit_Interrupt/STM32WLx_Transmit_Interrupt.ino
@@ -38,6 +38,18 @@ static const Module::RfSwitchMode_t rfswitch_table[] = {
 // save transmission state between loops
 int transmissionState = RADIOLIB_ERR_NONE;
 
+// flag to indicate that a packet was sent
+volatile bool transmittedFlag = false;
+
+// this function is called when a complete packet
+// is transmitted by the module
+// IMPORTANT: this function MUST be 'void' type
+//            and MUST NOT have any arguments!
+void setFlag(void) {
+  // we sent a packet, set the flag
+  transmittedFlag = true;
+}
+
 void setup() {
   Serial.begin(9600);
 
@@ -83,18 +95,6 @@ void setup() {
                       0x89, 0xAB, 0xCD, 0xEF};
     state = radio.startTransmit(byteArr, 8);
   */
-}
-
-// flag to indicate that a packet was sent
-volatile bool transmittedFlag = false;
-
-// this function is called when a complete packet
-// is transmitted by the module
-// IMPORTANT: this function MUST be 'void' type
-//            and MUST NOT have any arguments!
-void setFlag(void) {
-  // we sent a packet, set the flag
-  transmittedFlag = true;
 }
 
 // counter to keep track of transmitted packets
