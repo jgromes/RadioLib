ba3449e8a23fa0e5095465557a6d218e3c3a8349
jgromes <jan.gromes>
2024-12-31 12:21:06 +0100

    [CI] Add CI scripts, save metrics on build

---
+++ extras/test/ci/parse_size.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+board=$1
+hash=$(git rev-parse --short HEAD)
+
+in_file="/tmp/size_$board.txt"
+out_file="/tmp/size_${hash}_$board.csv"
+rm -f $out_file
+
+# write the header
+echo "text,data,bss,dec,hex,filename" > "$out_file"
+
+# convert to CSV
+awk 'NR > 1 {print $7 "," $8 "," $9 "," $10 "," $11 "," $12}' "$in_file" >> "$out_file"
+
+# remove input file
+rm -f $in_file
+
