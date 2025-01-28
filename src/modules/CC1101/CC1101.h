a4ec7cd919c2b30983888c93f13e6202b5676736
Crsarmv7l <85343771+Crsarmv7l@users.noreply.github.com>
2025-01-28 16:22:18 +0100

    Update CC1101.h
    Add Max packet size for FIFO Refills

--- src/modules/CC1101/CC1101.h
+++ src/modules/CC1101/CC1101.h
@@ -8,6 +8,7 @@
 
 // CC1101 physical layer properties
 #define RADIOLIB_CC1101_FREQUENCY_STEP_SIZE                     396.7285156
+#define RADIOLIB_CC1101_MAX_PACKET_LENGTH                       255
 #define RADIOLIB_CC1101_MAX_PACKET_LENGTH                       64
 #define RADIOLIB_CC1101_CRYSTAL_FREQ                            26.0f
 #define RADIOLIB_CC1101_DIV_EXPONENT                            16
