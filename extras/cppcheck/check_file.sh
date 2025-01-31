8ab989ab53d4d015776c8d72cc2d807ac2455810
jgromes <jan.gromes>
2025-01-31 18:25:38 +0100

    [CI] Add helper script to cppcheck single path

---
+++ extras/cppcheck/check_file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $@ -lt 1 ]]; then
+    echo "Usage: $0 <path to check>"
+    exit 1
+fi
+
+path=$1
+
+cppcheck --version
+cppcheck $path --enable=all \
+             --force \
+             --inline-suppr \
+             --suppress=ConfigurationNotChecked \
+             --suppress=unusedFunction \
+             --suppress=missingIncludeSystem \
+             --suppress=missingInclude \
+             --quiet
