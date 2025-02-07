f4e2b3e5b0b1daf7d27986915b9a460ad3ab3a45
jgromes <jan.gromes>
2025-02-07 21:22:32 +0100

    [CI] Ignore geninfo errors

--- extras/test/unit/coverage.sh
+++ extras/test/unit/coverage.sh
@@ -6,7 +6,7 @@ rm -rf $filename.*
 lcov --capture --directory build --output-file "${filename}.info"
 
 # filter out boost and C++ standard library
-lcov --remove "${filename}.info" "/usr/*/boost/*" "/usr/include/c++/*" --output-file "${filename}.info"
+lcov --remove "${filename}.info" "/usr/*/boost/*" "/usr/include/c++/*" --ignore-errors mismatch --output-file "${filename}.info"
 
 # generate HTML
 genhtml "${filename}.info" --output-directory "${filename}.report"
