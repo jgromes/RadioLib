d205b0dcff5f4d3978cf8923bb48ce3000d7f9a0
jgromes <jan.gromes>
2025-02-07 21:56:19 +0100

    [CI] Revert gcc-11

--- extras/test/unit/coverage.sh
+++ extras/test/unit/coverage.sh
@@ -3,7 +3,7 @@
 set -e
 filename="lcov"
 rm -rf $filename.*
-lcov --capture --directory build --output-file "${filename}.info" --gcov-tool gcov-11
+lcov --capture --directory build --output-file "${filename}.info"
 
 # filter out boost and C++ standard library
 lcov --remove "${filename}.info" "/usr/*/boost/*" "/usr/include/c++/*" --output-file "${filename}.info"
