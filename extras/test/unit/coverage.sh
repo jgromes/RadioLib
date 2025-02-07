da226bb91b097173b9bd9da3120b1c2f1518da8e
jgromes <jan.gromes>
2025-02-07 21:48:47 +0100

    [CI] Force gcov 11

--- extras/test/unit/coverage.sh
+++ extras/test/unit/coverage.sh
@@ -3,10 +3,10 @@
 set -e
 filename="lcov"
 rm -rf $filename.*
-lcov --capture --directory build --output-file "${filename}.info"
+lcov --capture --directory build --output-file "${filename}.info" --gcov-tool gcov-11
 
 # filter out boost and C++ standard library
-lcov --remove "${filename}.info" "/usr/*/boost/*" "/usr/include/c++/*" --ignore-errors mismatch --output-file "${filename}.info"
+lcov --remove "${filename}.info" "/usr/*/boost/*" "/usr/include/c++/*" --output-file "${filename}.info"
 
 # generate HTML
 genhtml "${filename}.info" --output-directory "${filename}.report"
