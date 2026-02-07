648dcaad118064ae4775d0cfbfdaf6edac9e160d
jgromes <jan.gromes@gmail.com>
2026-02-07 18:29:36 +0000

    [ADS-B] Initial WIP commit

---
+++ src/protocols/ADSB/ADSB.cpp
@@ -0,0 +1,134 @@
+#include "ADSB.h"
+
+#if !RADIOLIB_EXCLUDE_ADSB
+
+ADSBClient::ADSBClient(PhysicalLayer* phy) {
+  phyLayer = phy;
+}
+
+int16_t ADSBClient::begin() {
+  // set what is supported by PHY
+  int16_t state = phyLayer->setFrequency(RADIOLIB_ADSB_CARRIER_FREQUENCY);
+  RADIOLIB_ASSERT(state);
+
+  state = phyLayer->setPreambleLength(16);
+  RADIOLIB_ASSERT(state);
+  
+  state = phyLayer->setSyncWord(NULL, 0);
+  RADIOLIB_ASSERT(state);
+
+  state = phyLayer->setEncoding(RADIOLIB_ENCODING_MANCHESTER_INV);
+  return(state);
+}
+
+int16_t ADSBClient::decode(uint8_t in[RADIOLIB_ADSB_FRAME_LEN_BYTES], ADSBFrame* out) {
+  RADIOLIB_ASSERT_PTR(out);
+
+  // get the basic information
+  out->downlinkFormat = (in[0] & 0xF8) >> 3;
+  out->capability = in[0] & 0x07;
+  for(int i = 0; i < RADIOLIB_ADSB_FRAME_ICAO_LEN_BYTES; i++) {
+    out->icao[i] = in[RADIOLIB_ADSB_FRAME_ICAO_POS + i];
+  }
+
+  // lookup table to avoid a whole bunch of if-else statements
+  const ADSBMessageType msgTypeLut[] = {
+    ADSBMessageType::RESERVED,
+    ADSBMessageType::AIRCRAFT_ID, ADSBMessageType::AIRCRAFT_ID,
+    ADSBMessageType::AIRCRAFT_ID, ADSBMessageType::AIRCRAFT_ID, 
+    ADSBMessageType::SURFACE_POS, ADSBMessageType::SURFACE_POS, 
+    ADSBMessageType::SURFACE_POS, ADSBMessageType::SURFACE_POS, 
+    ADSBMessageType::AIRBORNE_POS_ALT_BARO, ADSBMessageType::AIRBORNE_POS_ALT_BARO, 
+    ADSBMessageType::AIRBORNE_POS_ALT_BARO, ADSBMessageType::AIRBORNE_POS_ALT_BARO, 
+    ADSBMessageType::AIRBORNE_POS_ALT_BARO, ADSBMessageType::AIRBORNE_POS_ALT_BARO, 
+    ADSBMessageType::AIRBORNE_POS_ALT_BARO, ADSBMessageType::AIRBORNE_POS_ALT_BARO, 
+    ADSBMessageType::AIRBORNE_POS_ALT_BARO, ADSBMessageType::AIRBORNE_POS_ALT_BARO, 
+    ADSBMessageType::AIRBORNE_VEL, 
+    ADSBMessageType::AIRBORNE_POS_ALT_GNSS,
+    ADSBMessageType::AIRBORNE_POS_ALT_GNSS,
+    ADSBMessageType::AIRBORNE_POS_ALT_GNSS,
+    ADSBMessageType::RESERVED, ADSBMessageType::RESERVED,
+    ADSBMessageType::RESERVED, ADSBMessageType::RESERVED, ADSBMessageType::RESERVED,
+    ADSBMessageType::AIRCRAFT_STATUS,
+    ADSBMessageType::TARGET_STATE,
+    ADSBMessageType::AIRCRAFT_OPS_STATUS,
+  };
+
+  // get the message type and then the message itself
+  out->messageType = msgTypeLut[in[RADIOLIB_ADSB_FRAME_MESSAGE_POS] & 0x1F];
+  for(int i = 0; i < RADIOLIB_ADSB_FRAME_MESSAGE_LEN_BYTES; i++) {
+    out->message[i] = in[RADIOLIB_ADSB_FRAME_MESSAGE_POS + i];
+  }
+
+  return(RADIOLIB_ERR_NONE);
+}
+
+int16_t ADSBClient::parseCallsign(ADSBFrame* in, char callsign[RADIOLIB_ADSB_CALLSIGN_LEN], ADSBAircraftCategory* cat) {
+  RADIOLIB_ASSERT_PTR(in);
+
+  if(in->messageType != ADSBMessageType::AIRCRAFT_ID) {
+    return(RADIOLIB_ERR_ADSB_INVALID_MSG_TYPE);
+  }
+
+  // precomputed bitshifts
+  callsign[0] = in->message[1] & (0x3F << 2) >> 2;
+  callsign[1] = (in->message[1] & (0x03 >> 0) << 4) | ((in->message[2] & (0x0F << 4)) >> 4);
+  callsign[2] = (in->message[2] & (0x0F << 0) << 2) | ((in->message[3] & (0x03 << 6)) >> 6);
+  callsign[3] = in->message[3] & 0x3F;
+  callsign[4] = in->message[4] & (0x3F << 2) >> 2;
+  callsign[5] = (in->message[4] & (0x03 >> 0) << 4) | ((in->message[5] & (0x0F << 4)) >> 4);
+  callsign[6] = (in->message[5] & (0x0F << 0) << 2) | ((in->message[6] & (0x03 << 6)) >> 6);
+  callsign[7] = in->message[6] & 0x3F;
+  callsign[RADIOLIB_ADSB_CALLSIGN_LEN - 1] = '\0';
+
+  // only continue processing if category was requested by user
+  if(!cat) { return(RADIOLIB_ERR_NONE);}
+  
+  uint8_t type = (in->message[0] & 0xF8) >> 3;
+  uint8_t category = in->message[0] & 0x03;
+  if(type < 2) {
+    *cat = ADSBAircraftCategory::RESERVED;
+    return(RADIOLIB_ERR_NONE);
+  } else if(category == 0) {
+    *cat = ADSBAircraftCategory::NONE;
+    return(RADIOLIB_ERR_NONE);
+  }
+
+  // lookup table to avoid a whole bunch of if-else statements
+  const ADSBAircraftCategory catLut[] = {
+    ADSBAircraftCategory::SURFACE_EMERGENCY,
+    ADSBAircraftCategory::RESERVED,
+    ADSBAircraftCategory::SURFACE_SERVICE,
+    ADSBAircraftCategory::GROUND_OBSTRUCTION,
+    ADSBAircraftCategory::GROUND_OBSTRUCTION,
+    ADSBAircraftCategory::GROUND_OBSTRUCTION,
+    ADSBAircraftCategory::GROUND_OBSTRUCTION,
+    ADSBAircraftCategory::GLIDER,
+    ADSBAircraftCategory::LIGHTER_THAN_AIR,
+    ADSBAircraftCategory::PARACHUTIST,
+    ADSBAircraftCategory::ULTRALIGHT_PARAGLIDER,
+    ADSBAircraftCategory::RESERVED,
+    ADSBAircraftCategory::UAV,
+    ADSBAircraftCategory::SPACE_VEHICLE,
+    ADSBAircraftCategory::LIGHT,
+    ADSBAircraftCategory::MEDIUM_1,
+    ADSBAircraftCategory::MEDIUM_2,
+    ADSBAircraftCategory::HIGH_VORTEX,
+    ADSBAircraftCategory::HEAVY,
+    ADSBAircraftCategory::HIGH_PERFORMANCE,
+    ADSBAircraftCategory::ROTORCRAFT,
+  };
+
+  // the index is parsed from the type and category
+  // in real world values overflowing the lookup table should not appear,
+  // but better to check anyway
+  size_t index = (type - 2)*7 + (category - 1);
+  if(index > sizeof(catLut)/sizeof(catLut[0])) {
+    return(RADIOLIB_ERR_ADSB_INVALID_CATEGORY);
+  }
+
+  *cat = catLut[index];
+  return(RADIOLIB_ERR_NONE);
+}
+
+#endif
---
+++ src/protocols/ADSB/ADSB.h
@@ -0,0 +1,131 @@
+#if !defined(RADIOLIB_ADSB_H)
+#define RADIOLIB_ADSB_H
+
+#include "../../TypeDef.h"
+
+#if !RADIOLIB_EXCLUDE_ADSB
+
+#include "../PhysicalLayer/PhysicalLayer.h"
+
+#define RADIOLIB_ADSB_CARRIER_FREQUENCY                         (1090.0f)
+
+// basic ADS-B frame structure
+#define RADIOLIB_ADSB_FRAME_LEN_BYTES                           (14)
+#define RADIOLIB_ADSB_FRAME_DF_CA_POS                           (0)
+#define RADIOLIB_ADSB_FRAME_ICAO_LEN_BYTES                      (3)
+#define RADIOLIB_ADSB_FRAME_ICAO_POS                            (1)
+#define RADIOLIB_ADSB_FRAME_MESSAGE_LEN_BYTES                   (7)
+#define RADIOLIB_ADSB_FRAME_MESSAGE_POS                         (4)
+#define RADIOLIB_ADSB_FRAME_PARITY_INTERROGATOR_LEN_BYTES       (3)
+#define RADIOLIB_ADSB_FRAME_PARITY_INTERROGATOR_POS             (11)
+
+// length of the callsign, including a terminating null
+#define RADIOLIB_ADSB_CALLSIGN_LEN                              (8 + 1)
+
+/*!
+  \enum ADSBMessageType
+  \brief ADS-B Message Type
+*/
+enum class ADSBMessageType {
+  AIRCRAFT_ID = 0,
+  SURFACE_POS,
+  AIRBORNE_POS_ALT_BARO,
+  AIRBORNE_VEL,
+  AIRBORNE_POS_ALT_GNSS,
+  AIRCRAFT_STATUS,
+  TARGET_STATE,
+  AIRCRAFT_OPS_STATUS,
+  RESERVED,
+};
+
+/*!
+  \enum ADSBAircraftCategory
+  \brief ADS-B Aircraft category
+*/
+enum class ADSBAircraftCategory {
+  NONE = 0,
+  SURFACE_EMERGENCY,
+  SURFACE_SERVICE,
+  GROUND_OBSTRUCTION,
+  GLIDER,
+  LIGHTER_THAN_AIR,
+  PARACHUTIST,
+  ULTRALIGHT_PARAGLIDER,
+  UAV,
+  SPACE_VEHICLE,
+  LIGHT,
+  MEDIUM_1,
+  MEDIUM_2,
+  HIGH_VORTEX,
+  HEAVY,
+  HIGH_PERFORMANCE,
+  ROTORCRAFT,
+  RESERVED,
+};
+
+/*!
+  \struct ADSBFrame
+  \brief ADS-B Frame structure
+*/
+struct ADSBFrame {
+  public:
+    /*! \brief Downlink format (DF). Received frames should always have DF = 17. */
+    uint8_t downlinkFormat;
+
+    /*! \brief Transponder capability. */
+    uint8_t capability;
+
+    /*! \brief Transponder ICAO address. */
+    uint8_t icao[RADIOLIB_ADSB_FRAME_ICAO_LEN_BYTES];
+
+    /*! \brief Message type. */
+    ADSBMessageType messageType;
+
+    /*! \brief Message buffer, interpretation is dependent on the value of messageType. */
+    uint8_t message[RADIOLIB_ADSB_FRAME_MESSAGE_LEN_BYTES];
+};
+
+/*!
+  \class ADSBClient
+  \brief Client for receiving and decoding ADS-B broadcast.
+*/
+class ADSBClient {
+  public:
+    /*! 
+      \brief Default constructor.
+      \param phy Pointer to the wireless module providing PhysicalLayer communication.
+    */
+    explicit ADSBClient(PhysicalLayer* phy);
+
+    /*!
+      \brief Initialization method.
+      \returns \ref status_codes
+    */
+    int16_t begin();
+
+    /*!
+      \brief Frame decoding method, turns raw received binary data into ADS-B frame.
+      \param in Received raw data.
+      \param out Pointer to ADSBFrame where the decoded frame will be saved.
+      \returns \ref status_codes
+    */
+    int16_t decode(uint8_t in[RADIOLIB_ADSB_FRAME_LEN_BYTES], ADSBFrame* out);
+
+    /*!
+      \brief Method to parse callsign from a received frame.
+      \param in Pointer to ADSBFrame where decoded frame was saved.
+      \param callsign Buffer where the parsed callsign will be saved as null-terminated string.
+      \param cat If set, parsed aircraft category will be saved here.
+      \returns \ref status_codes
+    */
+    int16_t parseCallsign(ADSBFrame* in, char callsign[RADIOLIB_ADSB_CALLSIGN_LEN], ADSBAircraftCategory* cat = NULL);
+  
+#if !RADIOLIB_GODMODE
+  private:
+#endif
+    PhysicalLayer* phyLayer;
+};
+
+#endif
+
+#endif
