ce3013408441d25a10f8075ae0188ddb16de3ac7
jgromes <jan.gromes@gmail.com>
2026-02-18 16:54:52 +0000

    [CI] Fix CI getting stuck on unit test

--- extras/test/unit/include/TestHal.hpp
+++ extras/test/unit/include/TestHal.hpp
@@ -140,14 +140,15 @@ class TestHal : public RadioLibHal {
       HAL_LOG("TestHal::delayMicroseconds(us=" << us << ")");
       const auto start = std::chrono::high_resolution_clock::now();
 
-      // busy wait is needed for microseconds precision
-      const auto len = std::chrono::microseconds(us);
-      while(std::chrono::high_resolution_clock::now() - start < len);
+      // this is incredibly hacky, but there is no reliable way for sub-ms sleeping
+      std::this_thread::sleep_for(std::chrono::duration<unsigned long, std::milli>(1));
 
       // measure and print
       const auto end = std::chrono::high_resolution_clock::now();
-      const std::chrono::duration<double, std::micro> elapsed = end - start;
-      HAL_LOG("TestHal::delayMicroseconds(us=" << us << ")=" << elapsed.count() << "us");
+      const std::chrono::duration<double, std::milli> elapsed = end - start;
+
+      HAL_LOG("TestHal::delayMicroseconds(us=" << us << ")=" << elapsed.count() << "ms");
+      (void)us;
     }
 
     void yield() override {
--- extras/test/unit/test.sh
+++ extras/test/unit/test.sh
@@ -12,8 +12,4 @@ make -j4
 # run it
 cd ..
 
-# TODO silly workaround to prevent this from getting stuck in CI
-./build/radiolib-unittest --log_level=message --detect_memory_leaks --run_test=suite_Module
-./build/radiolib-unittest --log_level=message --detect_memory_leaks --run_test=suite_TimeOnAir
-./build/radiolib-unittest --log_level=message --detect_memory_leaks --run_test=suite_PhyComplete
-./build/radiolib-unittest --log_level=message --detect_memory_leaks --run_test=suite_Crypto
+./build/radiolib-unittest --log_level=message --detect_memory_leaks
