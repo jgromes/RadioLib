6a13da7066a5e2b7de8c276a814345a3ad1b1cb1
rynskyi <rynskyi@gmail.com>
2026-05-10 01:20:01 +0300

    [LR11x0] Fix SF6 compatibility with SX127x

--- src/modules/LR11x0/LR11x0.cpp
+++ src/modules/LR11x0/LR11x0.cpp
@@ -565,9 +565,11 @@ int16_t LR11x0::setSpreadingFactor(uint8_t sf, bool legacy) {
 
   RADIOLIB_CHECK_RANGE(sf, 5, 12, RADIOLIB_ERR_INVALID_SPREADING_FACTOR);
 
-  // TODO enable SF6 legacy mode
   if(legacy && (sf == 6)) {
-    //this->mod->SPIsetRegValue(RADIOLIB_LR11X0_REG_SF6_SX127X_COMPAT, RADIOLIB_LR11X0_SF6_SX127X, 18, 18);
+    // Enable LR1121 SF6 compatibility with SX127x family
+    // Register 0xF20414: bit18 = 1, bit23 = 0
+    state = this->writeRegMemMask32(RADIOLIB_LR11X0_REG_SF6_SX127X_COMPAT, (0x1UL << 18) | (0x1UL << 23), RADIOLIB_LR11X0_SF6_SX127X);
+    RADIOLIB_ASSERT(state);
   }
 
   // update modulation parameters
