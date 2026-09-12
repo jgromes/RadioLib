a7c4e4f2f31d60eb0ead0d76a5701eb7fecd4229
jgromes <jan.gromes@gmail.com>
2026-09-12 18:13:40 +0100

    [AFSK] Drop floats from examples

--- examples/AFSK/AFSK_Imperial_March/AFSK_Imperial_March.ino
+++ examples/AFSK/AFSK_Imperial_March/AFSK_Imperial_March.ino
@@ -48,7 +48,7 @@ void setup() {
   // initialize SX1278 at 434 MHz
   Serial.print(F("[SX1278] Initializing ... "));
   ConfigFSK_t config;
-  config.frequency = 434;
+  config.frequency = RADIOLIB_UNIT_MEGA(434);
   int state = radio.beginFSK(config);
 
   // when using one of the non-LoRa modules for RTTY
