1f2f7737d9021426960cbd82858748743aa9c6c6
jgromes <jan.gromes@gmail.com>
2026-09-12 19:56:57 +0100

    [Si443x] Drop floats from examples

--- examples/Si443x/Si443x_Settings/Si443x_Settings.ino
+++ examples/Si443x/Si443x_Settings/Si443x_Settings.ino
@@ -47,7 +47,7 @@ void setup() {
   // initialize Si4432 at 434 MHz
   Serial.print(F("[Si4432] Initializing ... "));
   ConfigFSK_t config1;
-  config1.frequency = 434;
+  config1.frequency = RADIOLIB_UNIT_MEGA(434);
   int state = radio1.begin(config1);
   if (state == RADIOLIB_ERR_NONE) {
     Serial.println(F("success!"));
@@ -67,20 +67,20 @@ void setup() {
   // preamble length:             32 bits
   #if (__cplusplus < 201402L)
     ConfigFSK_t config2;
-    config2.frequency = 868.0;
-    config2.bitRate = 200.0;
-    config2.frequencyDeviation = 60.0;
-    config2.receiverBandwidth = 335.5;
+    config2.frequency = RADIOLIB_UNIT_MEGA(868);
+    config2.bitRate = RADIOLIB_UNIT_KILO(200);
+    config2.frequencyDeviation = RADIOLIB_UNIT_KILO(60);
+    config2.receiverBandwidth = RADIOLIB_UNIT_KILO(335);
     config2.power = 17;
     config2.preambleLength = 32;
     state = radio2.begin(config2);
   #else
     // with C++14 or newer, you can use named argument lists
     state = radio2.begin({
-      .frequency = 868.0,
-      .bitRate = 200.0,
-      .frequencyDeviation = 60.0,
-      .receiverBandwidth = 335.5,
+      .frequency = RADIOLIB_UNIT_MEGA(868),
+      .bitRate = RADIOLIB_UNIT_KILO(300),
+      .frequencyDeviation = RADIOLIB_UNIT_KILO(60),
+      .receiverBandwidth = RADIOLIB_UNIT_KILO(335),
       .power = 17,
       .preambleLength = 32
     });
@@ -97,13 +97,13 @@ void setup() {
   // and check if the configuration was changed successfully
 
   // set carrier frequency to 433.5 MHz
-  if (radio1.setFrequency(433.5) == RADIOLIB_ERR_INVALID_FREQUENCY) {
+  if (radio1.setFrequency(RADIOLIB_UNIT_MEGA(433)) == RADIOLIB_ERR_INVALID_FREQUENCY) {
     Serial.println(F("[Si4432] Selected frequency is invalid for this module!"));
     while (true) { delay(10); }
   }
 
   // set bit rate to 100.0 kbps
-  state = radio1.setBitRate(100.0);
+  state = radio1.setBitRate(RADIOLIB_UNIT_KILO(100));
   if (state == RADIOLIB_ERR_INVALID_BIT_RATE) {
     Serial.println(F("[Si4432] Selected bit rate is invalid for this module!"));
     while (true) { delay(10); }
@@ -114,14 +114,14 @@ void setup() {
   }
 
   // set receiver bandwidth to 284.8 kHz
-  state = radio1.setRxBandwidth(284.8);
+  state = radio1.setRxBandwidth(284800);
   if (state == RADIOLIB_ERR_INVALID_RX_BANDWIDTH) {
     Serial.println(F("[Si4432] Selected receiver bandwidth is invalid for this module!"));
     while (true) { delay(10); }
   }
 
   // set frequency deviation to 10.0 kHz
-  if (radio1.setFrequencyDeviation(10.0) == RADIOLIB_ERR_INVALID_FREQUENCY_DEVIATION) {
+  if (radio1.setFrequencyDeviation(RADIOLIB_UNIT_KILO(10)) == RADIOLIB_ERR_INVALID_FREQUENCY_DEVIATION) {
     Serial.println(F("[Si4432] Selected frequency deviation is invalid for this module!"));
     while (true) { delay(10); }
   }
--- examples/Si443x/Si443x_Transmit_Interrupt/Si443x_Transmit_Interrupt.ino
+++ examples/Si443x/Si443x_Transmit_Interrupt/Si443x_Transmit_Interrupt.ino
@@ -57,7 +57,7 @@ void setup() {
   // initialize Si4432 at 434 MHz
   Serial.print(F("[Si4432] Initializing ... "));
   ConfigFSK_t config;
-  config.frequency = 434;
+  config.frequency = RADIOLIB_UNIT_MEGA(434);
   int state = radio.begin(config);
   if (state == RADIOLIB_ERR_NONE) {
     Serial.println(F("success!"));
